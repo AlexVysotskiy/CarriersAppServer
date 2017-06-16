8120bac974649c2de265430703a6166c4a9a240c
Vysotskiy Alexander <vysotskij.av@gmail.com>
2017-06-16 11:16:37 +0300

    fix name length

---
+++ src/UserBundle/Controller/Api/AuthController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace UserBundle\Controller\Api;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+/**
+ * @Security("is_granted('ROLE_USER')")
+ */
+class AuthController extends Controller
+{
+
+    /**
+     * @Route("/user_info", name="api_v1_user_info")
+     * @Method("POST")
+     */
+    public function userInfoAction(Request $request)
+    {
+        
+        $response = new Response($this->serialize(array(
+                    'user' => $this->getUser()
+                )), Response::HTTP_OK);
+
+        return $this->setBaseHeaders($response);
+    }
+
+}
