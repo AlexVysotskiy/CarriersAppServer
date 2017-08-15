3cdf9f4da1439cdeba902c4004e8cf24ad05a0ec
Aleksandr Vysotckii <a.vysotckii@traso.de>
2017-08-15 09:34:48 +0200

    Fixes from Stas.

--- src/UserBundle/Controller/Api/CommonActionsController.php
+++ src/UserBundle/Controller/Api/CommonActionsController.php
@@ -51,7 +51,7 @@ class CommonActionsController extends Controller
         $user = $em->getRepository('UserBundle\Entity\User')->find($request->get('id'));
 
         $message = (new \Swift_Message('Запрос на обратный звонок от перевозчика #' . $user->getId() . '.'))
-            ->setFrom('Перевозчик: ' . $email->value)
+            ->setFrom($email->value, 'Перевозчик')
             ->setTo($email->value)
             ->setBody(
                 'Поступил запрос на обратный звонок от перевозчика #' .
@@ -89,7 +89,7 @@ class CommonActionsController extends Controller
         if ($text = trim($request->get('text'))) {
 
             $message = (new \Swift_Message('Запрос/отзыв на обратный звонок от перевозчика #' . $user->getId() . '.'))
-                ->setFrom('Перевозчик: ' .$email->value)
+                ->setFrom($email->value, 'Перевозчик')
                 ->setTo($email->value)
                 ->setBody(
                     'Поступил запрос/отзыв от перевозчика #' .
